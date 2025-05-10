2802f7a5b5e5625906e342f6bbfa4c9b2f444eff
sashiwan <samueliwanczuk@gmail.com>
2025-05-10 17:50:18 -0300

    initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+	// TODO:
+	// Naive approach:
+	// 1. Create a CSV reader that returns struct with the transfer data (or something similar)
+	// 2. Create the function to persist on a DB
+	// 3. Create the email sender function/service
+
+	// Event driven approach:
+	// 1. Define the representation of the events
+	// 2. Create the event store (or emulate it with a classic SQL, research)
+	// 3. Create the commands to persist the events
+	// 4. Create the query for retrieve the data
+	// 5. Create the CSV parser
+	// 6. Create the email service
+
+	return
+}
