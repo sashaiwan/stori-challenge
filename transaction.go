ae75d8cd42c38a30a38b9039b28837d760f599e4
sashiwan <samueliwanczuk@gmail.com>
2025-05-12 12:37:22 -0300

    add mapping method

--- transaction.go
+++ transaction.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"stori-challenge/database"
+	"time"
+)
+
 type TransactionType int
 
 const (
@@ -12,4 +17,22 @@ type Transaction struct {
 	Date   string
 	Amount float64
 	Type   TransactionType
+}
+
+func (t Transaction) ToDatabaseModel(accountID int) database.TransactionModel {
+	// We already ensure date integrity
+	date, _ := time.Parse("2006-01-02", t.Date)
+
+	transactionType := "credit"
+	if t.Type == Debit {
+		transactionType = "debit"
+	}
+
+	return database.TransactionModel{
+		ID:              t.Id,
+		AccountID:       accountID,
+		TransactionDate: date,
+		Amount:          t.Amount,
+		TransactionType: transactionType,
+	}
 }
