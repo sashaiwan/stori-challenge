3bcc0c28877ffda183c2f6e5ab101d492fdf7104
sashiwan <samueliwanczuk@gmail.com>
2025-05-14 02:40:20 -0300

    cleanup unnecessary code

--- main.go
+++ main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"stori-challenge/database"
+	"stori-challenge/server"
+	"stori-challenge/services"
 
 	"github.com/joho/godotenv"
 )
@@ -25,52 +29,29 @@ func main() {
 
 	godotenv.Load()
 
-	// TODO: make the file path setting more reliable
-	csvPath := os.Getenv("CSV_FILE_PATH")
-	if csvPath == "" {
-		// Should use filePath.Join
-		csvPath = "./data/txns.csv"
-	}
-
-	recipientMail := os.Getenv("RECIPIENT_EMAIL")
-	if recipientMail == "" {
-		fmt.Fprintf(os.Stderr, "A recipient mail must be provided")
-		os.Exit(1)
-	}
-
 	db, err := database.NewDB()
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error initializing database: %v\n", err)
+		log.Fatalf("Error initializing database: %v\n", err)
 	} else {
 		fmt.Println("DB initialized and ready")
 		defer db.Close()
 	}
 
-	account, err := db.GetOrCreateAccount(recipientMail)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error fetching or creating account: %v", err)
+	// TODO: create a config mechanism to unify env variables handling
+	emailService := &services.EmailService{}
+	transactionsService := &services.TransactionService{}
+	filesService := &services.FilesService{}
+	server := server.NewServer(emailService, transactionsService, filesService, db)
+
+	http.HandleFunc("/transactions/stats", server.TransactionStatsHandler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
 	}
 
-	transactions, err := processCSV(csvPath)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Error processing file: %v\n", err)
-		os.Exit(1)
-	}
-	fmt.Printf("Successfully processed %d transactions\n", len(transactions))
-
-	result := db.SaveTransactions(account.ID, getTransactionsModels(account.ID, transactions))
-	if result != nil {
-		fmt.Fprintf(os.Stderr, "Error saving transactions: %v\n", result)
-	} else {
-		fmt.Printf("Successfully saved %d transactions\n", len(transactions))
-	}
-
-	transactionStats := getTransactionStats(transactions)
-	fmt.Println(transactionStats)
-
-	mailErr := sendEmail(transactionStats, recipientMail)
-	if mailErr != nil {
-		fmt.Fprintf(os.Stderr, "Error sending email: %v\n", mailErr)
-		os.Exit(1)
+	log.Printf("Starting HTTP server on post %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("HTTP server error: ", err)
 	}
 }
