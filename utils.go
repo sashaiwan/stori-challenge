94068906428313340eec681d9281c5e86feed132
sashiwan <samueliwanczuk@gmail.com>
2025-05-14 00:11:49 -0300

    update imports

--- utils.go
+++ utils.go
@@ -1,9 +1,12 @@
 package main
 
-import "stori-challenge/database"
+import (
+	"stori-challenge/database"
+	"stori-challenge/models"
+)
 
 // This can be generic
-func getTransactionsModels(accountID int, transactions []Transaction) []database.TransactionModel {
+func getTransactionsModels(accountID int, transactions []models.Transaction) []database.TransactionModel {
 	models := make([]database.TransactionModel, len(transactions))
 
 	for i, t := range transactions {
